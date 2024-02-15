d3d1b88e4e94da8308d98c4d4e900142cdbf6833
devraider <cristeapaul.2015@gmail.com>
2024-02-15 21:01:59 +0200

    Repositories update

--- src/main/java/artradio/link/radio/dao/RadioSongRepository.java
+++ src/main/java/artradio/link/radio/dao/RadioSongRepository.java
@@ -1,9 +1,16 @@
 package artradio.link.radio.dao;
 
 import artradio.link.radio.entity.RadioSong;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Repository
+@RepositoryRestResource
 public interface RadioSongRepository extends JpaRepository<RadioSong, Long> {
+
+    Page<RadioSong> findByRadioName(@RequestParam("radio") String radio, Pageable pageable);
 }
+
--- src/main/java/artradio/link/radio/dao/RadioTypeRepository.java
+++ src/main/java/artradio/link/radio/dao/RadioTypeRepository.java
@@ -2,8 +2,9 @@ package artradio.link.radio.dao;
 
 import artradio.link.radio.entity.RadioType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(exported = false)
 public interface RadioTypeRepository extends JpaRepository<RadioType, Long> {
 }
